e25752e81969f3d5f0ddfe632a5a91a711ce09f6
Diyanadura Menula Sasen De Silva <rusirunavanjaya2004@gmail.com>
2024-11-09 15:31:49 +0530

    Example.java

---
+++ Example.java
@@ -0,0 +1,15 @@
+//Example.java
+class Example{
+	public static void main(String args[]){
+    System.out.println("Hello World");
+
+		System.out.println(1200.0);
+ 		System.out.println(120000000.0);
+
+		System.out.println(0.0012);
+		System.out.println(0.0000012);
+
+		System.out.println(1.2E3);  //1200.0
+		System.out.println(1.2E-3); //0.0012
+	}
+}
